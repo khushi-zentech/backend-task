3cee64c09d14f2459af99537693b3695a8741156
khushi-zentech <khushi.vaghela@zentechsolution.com>
2026-02-04 18:13:58 +0530

    [task-3]: fixed more reusable code

--- circular_queue.py
+++ circular_queue.py
@@ -9,16 +9,8 @@ User will be given choice to perform action from above four.
 Script should be kept running until user chooses to exit.
 """
 
-# initialize circular queue variables
-front = -1
-rear = -1
-
-queue_size = int(input("Enter size of Queue: "))
-queue = [None] * queue_size
-
-
 # define front operation to get front item from queue
-def get_front():
+def get_front(front, queue):
     if front == -1:
         print("\nQueue is Empty.")
         return
@@ -26,7 +18,7 @@ def get_front():
     return queue[front]
 
 # define rear operation to get last item from queue
-def get_rear():
+def get_rear(rear, queue):
     if rear == -1:
         print("\nQueue is Empty.")
         return
@@ -34,12 +26,10 @@ def get_rear():
     return queue[rear]
 
 # define enqueue operation to add item to queue
-def enqueue(item):
-    global front, rear, queue, queue_size
-
+def enqueue(item, front, rear, queue, queue_size):
     if (rear+1) % queue_size == front:
         print(f"\nQueue is Full.\nCurrent Queue: {queue}")
-        return
+        return front, rear
     elif front == -1:
         front = 0
         rear = 0
@@ -48,14 +38,14 @@ def enqueue(item):
 
     queue[rear] = item
     print(f"\nEnqueue item to Queue: {item}\nCurrent Queue: {queue}")
+    
+    return front, rear
 
 # define dequeue operation to remove item from queue
-def dequeue():
-    global front, rear, queue, queue_size
-
+def dequeue(front, rear, queue, queue_size):
     if front == -1:
         print("\nQueue is Empty.\nNo item to dequeue from Queue.")
-        return
+        return front, rear
     
     item = queue[front]
     queue[front] = None
@@ -68,8 +58,18 @@ def dequeue():
     else:
         front = (front + 1) % queue_size
 
+    return front, rear
+
 # define menu to interact with user
 def menu():
+    
+    # initialize circular queue variables
+    front = -1
+    rear = -1
+
+    queue_size = int(input("Enter size of Queue: "))
+    queue = [None] * queue_size
+
     while True:
         print("\nSelect an Operation to perform on Queue:\n")
         print("1. Front")
@@ -80,14 +80,14 @@ def menu():
         choice = input("\nEnter your choice (1-5): ")
         
         if choice == '1':
-            print(f"\nFront at item: {get_front()}, Front: {front}\nCurrent Queue: {queue}")
+            print(f"\nFront at item: {get_front(front, queue)}, Front: {front}\nCurrent Queue: {queue}")
         elif choice == '2':
-            print(f"\nRear at item: {get_rear()}, Rear: {rear}\nCurrent Queue: {queue}")
+            print(f"\nRear at item: {get_rear(rear, queue)}, Rear: {rear}\nCurrent Queue: {queue}")
         elif choice == '3':
             item = input("\nEnter item to enqueue on Queue: ")
-            enqueue(item)
+            front, rear = enqueue(item, front, rear, queue, queue_size)
         elif choice == '4':
-            dequeue()
+            front, rear = dequeue(front, rear, queue, queue_size)
         elif choice == '5':
             print("\nYou selected to exit.\nThank you!")
             break
