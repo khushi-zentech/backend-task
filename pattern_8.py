2aea3b4b7e5ee937c216f05f2acbc09e65624db2
khushi-zentech <khushi.vaghela@zentechsolution.com>
2026-02-13 14:45:05 +0530

    [task-24]: add time complexity

--- pattern_8.py
+++ pattern_8.py
@@ -9,7 +9,7 @@ Task - 24: Pattern - 8: Inverted Pyramid Pattern of *
 
 # define a function to print the inverted pyramid pattern
 def print_pattern(row):
-    for i in range(row, 0, -1):
+    for i in range(row, 0, -1): # time complexity: O(row^2)
         for j in range(row-1, i-1, -1):
             print(" ", end=" ")
         
@@ -17,8 +17,19 @@ def print_pattern(row):
             print("*", end=" ")
         print()
 
-# take input from user
-row = int(input("Enter the number of rows: "))
+# take input from user and validate it
+while True:
+    # time complexity: depends on how many iterations occur before the break 
+    # so, O(row) in worst case and O(1) in best case
+    
+    row = input("\nEnter number of rows: ")
+     
+    if row.isdigit():      
+      # call the function
+      print_pattern(int(row))
+        
+      break
+    else:
+         print("\nPlease enter a valid positive integer input only.")
 
-# call the function
-print_pattern(row)
+# total time complexity: O(row^2) + O(row) = O(row^2)
