a67e483268a5ea5b89e9c149b7cf4220e5a56477
khushi-zentech <khushi.vaghela@zentechsolution.com>
2026-02-13 14:41:52 +0530

    [task-18]: add time complexity

--- pattern_2.py
+++ pattern_2.py
@@ -9,13 +9,24 @@ Task - 18: Pattern - 2: Right Angle Triangle Pattern of Numbers
 
 # define function to print the pattern
 def print_pattern(row):
-    for i in range(row+1):
-        for j in range(1, i+1):
+    for i in range(row+1): # time complexity: O(row^2)
+        for j in range(1, i+1): 
             print(j, end=" ")
         print()
 
-# take input from user
-row = int(input("Enter number of rows: "))
+# take input from user and validate it
+while True:
+    # time complexity: depends on how many iterations occur before the break 
+    # so, O(row) in worst case and O(1) in best case
 
-# call the function
-print_pattern(row)
+    row = input("\nEnter number of rows: ")
+
+    if row.isdigit():
+        # call the function
+        print_pattern(int(row))
+
+        break
+    else:
+        print("\nPlease enter a valid positive integer input only.")
+
+# total time complexity: O(row^2) + O(row) = O(row^2)
