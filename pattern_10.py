6f9dd529c974cd33dd3766c6168d5277370b683c
khushi-zentech <khushi.vaghela@zentechsolution.com>
2026-02-13 14:45:54 +0530

    [task-26]: add time complexity

--- pattern_10.py
+++ pattern_10.py
@@ -10,14 +10,26 @@ Task - 26: Pattern - 10: Right Angle Triangle Pattern of Consecutive Numbers
 def print_pattern(row):
     num = 1
     
-    for i in range(row+1):
+    for i in range(row+1): # time complexity: O(row^2)
         for j in range(i):
             print(num, end=" ")
             num += 1
+        
         print()
 
-# take input from user
-row = int(input("Enter number of rows: "))
+# take input from user and validate it
+while True:
+    # time complexity: depends on how many iterations occur before the break 
+    # so, O(row) in worst case and O(1) in best case
+    
+    row = input("\nEnter number of rows: ")
+     
+    if row.isdigit():
+        # call the function
+        print_pattern(int(row))
+         
+        break
+    else:
+        print("\nPlease enter a valid positive integer input only.")
 
-# call the function
-print_pattern(row)
+# total time complexity: O(row^2) + O(row) = O(row^2)
