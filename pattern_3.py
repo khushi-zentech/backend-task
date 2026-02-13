7e5c26065340895d44be800ecb3b7b4a062c5fe0
khushi-zentech <khushi.vaghela@zentechsolution.com>
2026-02-13 14:42:20 +0530

    [task-19]: add time complexity

--- pattern_3.py
+++ pattern_3.py
@@ -11,14 +11,25 @@ E E E E E
 def print_pattern(row):
     ch = 65
     
-    for i in range(row):
+    for i in range(row): # time complexity: O(row^2)
         for j in range(i+1):
             print(chr(ch), end=" ")
         ch += 1
         print()
 
-# take input from user
-row = int(input("Enter number of rows: "))
+# take input from user and validate it
+while True:
+    # time complexity: depends on how many iterations occur before the break 
+    # so, O(row) in worst case and O(1) in best case
+    
+    row = input("\nEnter number of rows: ")
+    
+    if row.isdigit():
+        # call the function
+        print_pattern(int(row))
+        
+        break
+    else:
+        print("\nPlease enter a valid positive integer input only.")
 
-# call the function
-print_pattern(row)
+# total time complexity: O(row^2) + O(row) = O(row^2)
