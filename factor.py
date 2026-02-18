c66343a3bace08ef3294f2ada072ccdbb3bc4eec
khushi-zentech <khushi.vaghela@zentechsolution.com>
2026-02-18 15:04:14 +0530

    [task-14]: add feature to find factor of a negative number

--- factor.py
+++ factor.py
@@ -2,11 +2,14 @@
 Task - 14: Find the factors of a number, optimize it as much as you can.
 """
 
+# import sqrt function from math module 
+from math import sqrt
+
 # define function to get factors of a number
 def get_factors(number):
     factor_list = []
 
-    for i in range(1, int(number ** 0.5)+1):
+    for i in range(1, int(sqrt(number)) + 1):
         if number%i == 0:
             factor_element = int(number/i)
 
@@ -20,13 +23,28 @@ def get_factors(number):
 
 # take input from user and validate it
 while True:
-    number = input("\nEnter number to find their factors: ")
+    try:
+        number = int(input("\nEnter number to find their factors: "))
 
-    if number.isdigit():
-        # call the function
-        result = get_factors(int(number))
-        
-        print(f"\nFactors of {number}: {result}")
-        break
-    else:
-        print("\nPlease enter a valid integer input only.")
+        if number < 0:
+            number = abs(number)
+            
+            # call the function to get factors
+            result = get_factors(number)
+
+            for i in range(len(result)):
+                result.append(-(result[i]))
+
+            print(f"\nFactors of {number}: {sorted(result)}")
+            
+            break
+        else:
+            # call the function to get factors
+            result = get_factors(number)
+            print(f"\nFactors of {number}: {result}")  
+            
+            break  
+    except ValueError:
+        print("\nValueError: Enter valid integer input only.")
+    except Exception:
+        print("\nPlease enter valid integer input only.")
