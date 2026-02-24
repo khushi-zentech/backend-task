0a69773e4ccf3444cd7fbcc66783f6ab58218e88
khushi-zentech <khushi.vaghela@zentechsolution.com>
2026-02-24 15:44:39 +0530

    [task-20]: fixed indentation of code

--- pattern_4.py
+++ pattern_4.py
@@ -19,14 +19,14 @@ while True:
     # time complexity: depends on how many iterations occur before the break 
     # so, O(row) in worst case and O(1) in best case
     
-     row = input("\nEnter number of rows: ")
+    row = input("\nEnter number of rows: ")
      
-     if row.isdigit():
-         # call the function
-         print_pattern(int(row))
+    if row.isdigit():
+        # call the function
+        print_pattern(int(row))
          
-         break
-     else:
-         print("\nPlease enter a valid positive integer input only.")
+        break
+    else:
+        print("\nPlease enter a valid positive integer input only.")
 
 # total time complexity: O(row^2) + O(row) = O(row^2)
